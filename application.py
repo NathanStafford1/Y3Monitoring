6d4fbaf5e690be8080022a0c03c3dabfab87d3a5
John Loane <john.loane@dkit.ie>
2022-10-04 13:03:51 +0100

    Working towards persistence
    Have added validation of the sport selected, better error checking
    and moving towards persistence

--- application.py
+++ application.py
@@ -2,15 +2,30 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = ["Basketball", "Soccer", "Tennis", "Snooker"]
 
-@app.route("/")
+REGISTRANTS = {}
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html", name=request.args.get("name", "World"))
+    if request.method == "GET":
+        return render_template("index.html", sports=SPORTS)
+    elif request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name", "world"))
 
 
-@app.route("/greet", methods=["POST"])
-def greet():
-    return render_template("greet.html", name=request.form.get("name", "world"))
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form.get("name")
+    if not name:
+        return render_template("error.html", message="Missing name")
+    sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
+    if sport not in SPORTS:
+        return render_template("error.html", message="Stop hacking my site")
+    REGISTRANTS[name] = sport
+    return render_template("registrants.html", registrants = REGISTRANTS)
 
 
 if __name__ == '__main__':
