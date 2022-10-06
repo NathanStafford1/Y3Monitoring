0ecf36ae4152cb8501e2d7c061c3539554d4521c
John Loane <john.loane@dkit.ie>
2022-10-06 14:14:03 +0100

    Added support for Mail
    Using flask_mail to send email using gmail account

--- application.py
+++ application.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+from flask_mail import Mail, Message
+import os
 
 app = Flask(__name__)
 
-SPORTS = ["Basketball", "Soccer", "Tennis", "Snooker"]
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = 'sd3a_registrants_23'
 
-REGISTRANTS = {}
+app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.getenv("GMAIL_APP_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+
+mail = Mail(app)
+
+
+mysql = MySQL(app)
+
+SPORTS = ["Basketball", "Soccer", "Tennis", "Snooker"]
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -16,16 +34,29 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    name = request.form.get("name")
-    if not name:
-        return render_template("error.html", message="Missing name")
+    email = request.form.get("email")
+    if not email:
+        return render_template("error.html", message="Missing email")
     sport = request.form.get("sport")
     if not sport:
         return render_template("error.html", message="Missing sport")
     if sport not in SPORTS:
         return render_template("error.html", message="Stop hacking my site")
-    REGISTRANTS[name] = sport
-    return render_template("registrants.html", registrants = REGISTRANTS)
+    cursor = mysql.connection.cursor()
+    cursor.execute('''insert into registrants(name, sport) values (%s, %s)''', (email, sport))
+    mysql.connection.commit()
+    cursor.close()
+    message = Message("You are registered", recipients=[email])
+    mail.send(message)
+    return redirect("/registrants")
+
+
+@app.route("/registrants")
+def registrants():
+    cur = mysql.connection.cursor()
+    cur.execute("select * from registrants")
+    registrants = cur.fetchall()
+    return render_template("registrants.html", registrants=registrants)
 
 
 if __name__ == '__main__':
